52ea5392f098350f161f1efdd64aa76fa1a4198d
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-02-09 23:20:48 -0300

    feat: virtualization

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,7 @@
+import { List, ListRowRenderer } from 'react-virtualized'
+
 import { ProductItem } from "./ProductItem"
+
 
 interface SearchResultsProps {
   results: Array<{
@@ -13,16 +16,35 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results, formattedTotalPrice, onAddToWishlist }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>Total Price: {formattedTotalPrice}</h2>
-      {results.map((product) => (
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+      {/* {results.map((product) => (
         <ProductItem
           key={product.id}
           product={product}
           onAddToWishlist={onAddToWishlist}
         />
-      ))}
+      ))} */}
     </div>
   )
 }
